460b45d8914900eed7248143b4526b7f46541714
hirokobe26 <hiro_kobe_26@yahoo.com>
2018-09-01 08:49:58 +0900

    modified the action in the order of controller-regulation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
@@ -11,13 +19,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
-    respond_to do |format|
-      format.html
-      format.json
-    end
-  end
 
   private
 
