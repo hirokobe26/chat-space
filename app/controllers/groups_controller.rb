266e8a9c2844b42aa5c37c373e6f2ff1ad81df12
hirokobe26 <hiro_kobe_26@yahoo.com>
2018-09-01 17:49:19 +0900

    現在のユーザーが自動的にグループに入るように設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,8 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(group_params).merge(id: current_user.id)
+    @group = Group.new(name: group_params[:name], user_ids: group_params[:user_ids])
+    @group.users << current_user
     if @group.save
       redirect_to "/groups/#{@group.id}/messages", notice: 'グループを作成しました'
     else
